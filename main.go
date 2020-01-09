76743f1bd83a71c1655051b0ffcf362726b4c325
imverbatim <ajdelco22@gmail.com>
2020-01-08 19:16:34 -0500

    yuh

---
+++ main.go
@@ -0,0 +1,59 @@
+// Copyright 2013 The Gorilla WebSocket Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"flag"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+var addr = flag.String("addr", ":8080", "http service address")
+
+func serveHome(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.Error(w, "Not found", http.StatusNotFound)
+		return
+	}
+	if r.Method != "GET" {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	http.ServeFile(w, r, "home.html")
+}
+
+func serveRoom(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	room := vars["room"]
+	if room == "" {
+		http.Error(w, "Not found", http.StatusNotFound)
+		return
+	}
+	if r.Method != "GET" {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	http.ServeFile(w, r, room)
+}
+
+func main() {
+	flag.Parse()
+	go h.run()
+	r := mux.NewRouter()
+	r.HandleFunc("/", serveHome)
+	r.HandleFunc("/{room}", serveRoom)
+	r.HandleFunc("/{room}/ws", func(w http.ResponseWriter, r *http.Request) {
+		serveWs(w, r)
+	})
+
+	http.Handle("/", r)
+	err := http.ListenAndServe(*addr, r)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
